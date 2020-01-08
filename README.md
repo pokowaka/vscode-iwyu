50777098b7065ba89d71839428228744f6632763
Erwin Jansen <jansene@google.com>
2020-01-08 11:35:23 -0800

    Detect compile_command.json changes.
    - Adds a few bug fixes around properties.
    - Improve messaging, just print number of files changed.
    - Remove "why" option as it breaks automated fixing.
    - Updates the documentation.
    - Lower minimum required version of vscode
    - Bump version number.

--- README.md
+++ README.md
@@ -10,8 +10,6 @@ Optimize the include files for the current C/C++ file:
 
 ![Include What You Use](images/small_demo.gif)
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
-
 ## Requirements
 
 You will need to install [include-what-you-use](https://include-what-you-use.org/). For example on mac:
@@ -20,24 +18,41 @@ You will need to install [include-what-you-use](https://include-what-you-use.org
 brew install include-what-you-use
 ```
 
+You must have a Python interpreter on the path that will be used. Both python 2/3 should work. This extension includes
+code from include-what-you-use, which uses the LLVM license. See the [LICENSE](LICENSE) file for details.
+
 ## Extension Settings
 
 This extension contributes the following settings:
 
 - `iwyu.exe`: Path to the include what you use executable.
 - `compile_commands` Path to compile_commands.json file.
-- `iwyu.mapping_files`: List of mapping files to use.
+- `iwyu.mapping_file`: Mapping file to use. See [IWYU Mappings](https://github.com/include-what-you-use/include-what-you-use/blob/master/docs/IWYUMappings.md) for details.
 - `iwyu.transitive_includes_only`: Do not suggest that a file add foo.h unless foo.h is already visible in the file's transitive includes.
 - `iwyu.max_line_length`: Maximum line length for includes.Note that this only affects comments and alignment thereof, the maximum line length can still be exceeded with long file names
-- `iwyu.comments`: Add 'why' comments.
-- `iwy.additional_params`: Additional parameters you wish to pass to iwyu.  Must be prefixed with a `-Xiwyu` flag
+- `iwyu.no_default_mappings`: Do not add iwyu's default mappings.
+- `iwyu.no_fwd_decls`: Do not use forward declarations.
+- `iwyu.keep`: A glob that tells iwyu to always keep these includes. Can be provided multiple times.
+- `iwyu.additional_params`: Additional parameters you wish to pass to iwyu. Must be prefixed with a `-Xiwyu` flag
 
 ## Known Issues
 
-- Changes in compile_commands.json are not detected.
+- You will need a python interpreter on the path.
 
 ## Release Notes
 
+### 1.0.2
+
+- Fixed typo in mapping_file parameter.
+
+### 1.0.1
+
+- Add some additional properties.
+- Removed `mapping_files` property and replaced it with single mapping file that can be set using `iwyu.mapping_file`
+- Removed `why` option, as it can break automated fixing.
+- Parse `compile_commands.json` when a change has been detected.
+- Mention that a python interpeter is needed.
+- Lower minimum version of vscode to 1.38.0
 
 ### 1.0.0
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,38 +1,89 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import {
-  CodeLens,
-  commands,
-  DecorationOptions,
-  DecorationRangeBehavior,
-  DecorationRenderOptions,
-  ExtensionContext,
-  OverviewRulerLane,
-  Position,
-  Progress,
-  ProgressLocation,
-  ProviderResult,
-  QuickPickItem,
-  Range,
-  StatusBarAlignment,
-  TextDocument,
-  TextEditor,
-  TextEditorDecorationType,
-  ThemeColor,
-  Uri,
-  window,
-  workspace
-} from "vscode";
+import { workspace } from "vscode";
 import * as fs from "fs";
 import { resolve } from "path";
 import * as cp from "child_process";
+import { promisify } from "util";
+import { EOL } from "os";
 
 type cc_entry = {
   directory: string;
   command: string;
   file: string;
 };
+
+function parseCompileCommands(
+  compile_commands_json: string
+): { [id: string]: cc_entry } {
+  console.log("Parsing: " + compile_commands_json);
+  let compile_commands = JSON.parse(
+    fs.readFileSync(compile_commands_json, "utf8")
+  ) as [cc_entry];
+
+  // Now iterate through the array and build up a map from file->command
+  var cc: { [id: string]: cc_entry } = {};
+  for (let entry of compile_commands) {
+    cc[entry.file] = entry;
+  }
+
+  return cc;
+}
+
+function runIwyu(
+  compile_command: cc_entry,
+  config: vscode.WorkspaceConfiguration
+) {
+  let file: string = compile_command.command.substr(
+    compile_command.command.indexOf(" ")
+  );
+  let exe: string = config.get<string>("exe", "include-what-you-use");
+  let len: number = config.get<number>("max_line_length", 80);
+  let keep: string = config
+    .get<Array<string>>("keep", [])
+    .map(x => "-Xiwyu --keep=" + x)
+    .join(" ");
+
+  // TODO(pokowaka): We could generalize this..
+  let trans: string = config.get<boolean>("transitive_includes_only", true)
+    ? "-Xiwyu --transitive_includes_only"
+    : "";
+  let no_fwd_decls: string = config.get<boolean>("no_fwd_decls", false)
+    ? "-Xiwyu --no_fwd_decls"
+    : "";
+  let no_default_mappings: string = config.get<boolean>(
+    "no_default_mappings",
+    false
+  )
+    ? "-Xiwyu --no_default_mappings"
+    : "";
+
+  var mapping: string = "";
+  if (config.get<string>("mapping_file", "").trim() !== "") {
+    mapping = "-Xiwyu --mapping_file=" + config.get<string>("mapping_file", "");
+  }
+
+  let additional: string = config.get<string>("additional_params", "");
+  let pyscript = resolve(__dirname, "fix_includes.py");
+  var cmd: string = `${exe} -Xiwyu --max_line_length=${len} ${trans} ${mapping} ${keep} ${additional} ${no_fwd_decls} ${no_default_mappings} ${file} 2>&1 | ${pyscript}`;
+
+  console.log(
+    "Running: pushd " + compile_command.directory + ";" + cmd + "; popd"
+  );
+
+  cp.exec(cmd, { cwd: compile_command.directory }, (err, stdout, stderr) => {
+    // Only first few lines have useful things..
+    console.log(stdout);
+    let of_intereset = stdout
+      .split(EOL)
+      .filter((e, i, a) => {
+        return e.includes("IWYU");
+      })
+      .join(EOL);
+    vscode.window.showInformationMessage(of_intereset);
+  });
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -52,54 +103,35 @@ export function activate(context: vscode.ExtensionContext) {
     .replace("${workspaceFolder}", wfd)
     .replace("${workspaceFolderBasename}", wfd);
 
-  let compile_commands = JSON.parse(
-    fs.readFileSync(compile_commands_json, "utf8")
-  ) as [cc_entry];
-
-  // Now iterate through the array and build up a map from file->command
-  var cc: { [id: string]: cc_entry } = {};
-  for (let entry of compile_commands) {
-    cc[entry.file] = entry;
-  }
+  let stat = promisify(fs.stat);
+  var cc_mtime: Number = 0;
+  stat(compile_commands_json).then(stats => {
+    cc_mtime = stats.mtimeMs;
+  });
+  var cc: { [id: string]: cc_entry } = parseCompileCommands(
+    compile_commands_json
+  );
 
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand("extension.iwyu", () => {
     // The code you place here will be executed every time your command is executed
     let editor = vscode.window.activeTextEditor;
+    let config = workspace.getConfiguration("iwyu");
     if (!editor) {
       return;
     }
-
     editor.document.save();
+    let fname = editor.document.fileName;
 
-    let config = workspace.getConfiguration("iwyu");
-
-    let compile_command = cc[editor.document.fileName];
-    let file: string = compile_command.command.substr(
-      compile_command.command.indexOf(" ")
-    );
-    let exe: string = config.get<string>("exe", "include-what-you-use");
-    let len: number = config.get<number>("max_line_length", 80);
-    let why: string = config.get<boolean>("comments", true)
-      ? ""
-      : "-Xiwyu --no_comments";
-    let trans: string = config.get<boolean>("transitive_includes_only", true)
-      ? "-Xiwyu --transitive_includes_only"
-      : "";
-    let mapping: string = config
-      .get<Array<string>>("mapping_files", [])
-      .map(x => "-Xiwyu --mapping_file=" + x)
-      .join(" ");
-    let additional: string = config.get<string>("additional_params", "");
-    let pyscript = resolve(__dirname, 'fix_includes.py');
-    var cmd: string = `${exe} -Xiwyu --max_line_length=${len} ${why} ${trans} ${mapping} ${additional} ${file} 2>&1 | ${pyscript}`;
-
-    console.log("Running: " + cmd);
-    //vscode.window.showInformationMessage('Using ' + exe);
-    cp.exec(cmd, { cwd: compile_command.directory }, (err, stdout, stderr) => {
-      vscode.window.showInformationMessage(stdout);
-     });
+    // Do we need to update our cc?
+    stat(compile_commands_json).then(stats => {
+      if (cc_mtime !== stats.mtimeMs) {
+        cc = parseCompileCommands(compile_commands_json);
+      }
+      cc_mtime = stats.mtimeMs;
+      runIwyu(cc[fname], config);
+    });
   });
 
   context.subscriptions.push(disposable);
